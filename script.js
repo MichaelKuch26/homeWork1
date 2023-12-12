0ac1a78d5aea89cd60fe45246b35bd604247a94d
MichaelKuch26 <mickey22121989@gmail.com>
2023-12-12 21:12:56 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// Task 1
+
+let tempC = 20;
+let tempF = (9/5)*tempC+32;
+console.log(`Температура в градусах Цельсиях: ${tempC}`);
+console.log(`Температура в Фаренгейтах: ${tempF}`);
+
+// Task 2
+const myName = 'Michael';
+let admin = myName;
+console.log(admin);
